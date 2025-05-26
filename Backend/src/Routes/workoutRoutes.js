abcbaff75fb527d8b0f2635f358d1000f72d8d93
ChenouWu <155577614+ChenouWu@users.noreply.github.com>
2025-05-26 15:31:15 +0800

    Update workoutRoutes.js

--- Backend/src/Routes/workoutRoutes.js
+++ Backend/src/Routes/workoutRoutes.js
@@ -89,6 +89,28 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// DELETE /api/workout/delete/:id
+router.delete("/delete/:id", protectRoutes, async (req, res) => {
+  try {
+    const workout = await Workout.findById(req.params.id);
+
+    if (!workout) {
+      return res.status(404).json({ message: "Workout not found" });
+    }
+
+    if (workout.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "You are not authorized to delete this workout" });
+    }
+
+    await workout.deleteOne(); //
+    res.status(200).json({ message: "Workout deleted successfully." });
+
+  } catch (error) {
+    console.error("Error deleting workout:", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 
   
